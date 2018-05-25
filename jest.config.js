5311f4c559238d337e183f8b5e3ee3360b1f6dc7
wesley <wesleyyw8@gmail.com>
2018-05-25 15:54:12 +0300

    tests coverage specified

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,8 @@ module.exports = {
   verbose: true,
   'collectCoverageFrom': [
     'client/actions/actionCreators.js',
-    'client/components/*-connectors.js',
-    'client/reducers/**.*.js',
-    '!**/node_modules/**'
+    'client/reducers/**',
+    '!client/reducers/index.js'
   ],
   moduleNameMapper: {
     '^actions[/](.+)': '<rootDir>/client/actions/$1',
